521ca6e07dd9cbcfa7e8178c10688b82f4c6b89f
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-06 14:48:49 -0600

    cbhj

--- main.js
+++ main.js
@@ -12,20 +12,23 @@ function searchClicked() {
   // Test Input Variable and update the page
   if (name === 'po' || name === 'dragon warrior') {
     // Update page to Po
-    document.getElementById('character-name').innerHTML = 'Po';
-    document.getElementById('main-img').src = 'img/po.png';
-    document.getElementById('quote').innerHTML =
-      'Buddy, I am the Dragon Warrior.';
+    // document.getElementById('character-name').innerHTML = 'Po';
+    // document.getElementById('main-img').src = 'img/po.png';
+    // document.getElementById('quote').innerHTML =
+    //   'Buddy, I am the Dragon Warrior.';
+    setCharacter("po", "Buddy, I am the Dragon Warrior")
   } else if (name === 'tigress' || name === 'master tigress') {
     // Update page to Tigress
-    document.getElementById('character-name').innerHTML = 'Master Tigress';
-    document.getElementById('main-img').src = 'img/tigress.png';
-    document.getElementById('quote').innerHTML = 'That was pretty hardcore!';
+    // document.getElementById('character-name').innerHTML = 'Master Tigress';
+    // document.getElementById('main-img').src = 'img/tigress.png';
+    // document.getElementById('quote').innerHTML = 'That was pretty hardcore!';
+    setCharacter("tigress", "That was pretty hardcore!")
   } else if (name === 'mantis' || name === 'master mantis') {
     // Update page to Mantis
-    document.getElementById('character-name').innerHTML = 'Master Mantis';
-    document.getElementById('main-img').src = 'img/mantis.png';
-    document.getElementById('quote').innerHTML = 'Fear the bug!';
+    // document.getElementById('character-name').innerHTML = 'Master Mantis';
+    // document.getElementById('main-img').src = 'img/mantis.png';
+    // document.getElementById('quote').innerHTML = 'Fear the bug!';
+    setCharacter("mantis", "Fear the bug!")
   } else if (name === 'monkey' || name === 'master monkey') {
     // Update page to Monkey
     document.getElementById('character-name').innerHTML = 'Master Monkey';
@@ -60,4 +63,10 @@ function searchClicked() {
     document.getElementById('main-img').src = 'img/question-mark.png';
     document.getElementById('quote').innerHTML = 'Character Not Found';
   }
+}
+
+function setCharacter(characterName, characterQuote) {
+  document.getElementById('character-name').innerHTML = characterName;
+    document.getElementById('main-img').src = `img/${characterName}.png`;
+    document.getElementById('quote').innerHTML = characterQuote;
 }
